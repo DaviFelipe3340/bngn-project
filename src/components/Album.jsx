c224378eaa58e42f2aa009581a01a06d21601007
DaviFelipe3340 <davifelipe2862@gmail.com>
2025-06-22 20:02:10 -0300

    sextou!

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -5,7 +5,7 @@ import AfilhadaVirtual from '../assets/afilhadaVirtual.jpeg';
 import Jampa from '../assets/jampa.jpeg';
 import ParmegianaRuim from '../assets/parmegianaRuim.jpeg';
 import FormaturaCeci from '../assets/formaturaCeci.jpeg'; 
-
+import Sextou from '../assets/sextou.jpeg';
 
 export default function Album() {
   const memories = [
@@ -34,6 +34,12 @@ export default function Album() {
       img: FormaturaCeci,
       title: 'FORMATURA DA MAIOR PESQUEIRA',
       text: 'Hoje é um daqueles dias que ficam guardados na memória pra sempre, ver você conquistando e fechando mais esse ciclo é algo que me deixa muito feliz. Você merece tudo de bom que a vida tem a oferecer, e eu estarei sempre aqui para te apoiar e torcer por você. Parabéns pela formatura amor!(XD) Você é uma pessoa incrível e tenho certeza de que ainda vai conquistar muitas coisas na vida. Te amo muito! ❤️'
+    },
+
+    {
+      img: Sextou,
+      title: 'Sextou!',
+      text: 'compartilhando do mesmo neorônio,é realmente a melhor do mundo no stop SINIIIIIIIIISSSSTRO. Primeira de muitas (se vc quiser) da melhor chamada da minha vida. DAVIXEROS'
     }
     
   ];
